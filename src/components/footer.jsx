bc1f6feed4838b54bebacd5e15c138240fcf7778
Filip Olczyk <261501@edu.p.lodz.pl>
2026-09-15 13:57:04 +0200

    Add target and rel attributes to footer links for improved security and usability

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,8 +3,8 @@ import youtubeIcon from './icons/icons8-youtube-32.png';
 const Footer = () => {
   return (
     <footer>
-        <a href="https://discord.gg/XJRYwZSPs6"><img src={discordIcon} alt="Discord Icon" /></a>
-        <a href="https://www.youtube.com/@Yoji720"><img src={youtubeIcon} alt="YouTube Icon" /></a>
+        <a href="https://discord.gg/XJRYwZSPs6" target="_blank" rel="noopener noreferrer"><img src={discordIcon} alt="Discord Icon" /></a>
+        <a href="https://www.youtube.com/@Yoji720" target="_blank" rel="noopener noreferrer"><img src={youtubeIcon} alt="YouTube Icon" /></a>
       <p>© 2026 LMU Stats Page</p>
     </footer>
   );
